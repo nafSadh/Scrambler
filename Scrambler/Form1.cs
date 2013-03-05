e6f3c21ed5b5f93236a8c0b1eba81be3fe47e0ca
kalashnikovisme <kalashnikovisme@gmail.com>
2013-03-05 04:35:37 +0400

    format code

--- Scrambler/Form1.cs
+++ Scrambler/Form1.cs
@@ -7,45 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Scrambler
-{
-    public partial class Form1 : Form
-    {
-        bool inputAnyText = false;
-        public Form1()
-        {
-            InitializeComponent();
-            inputText.Focus();
-        }
+namespace Scrambler {
+	public partial class Form1 : Form {
+		bool inputAnyText = false;
+		public Form1() {
+			InitializeComponent();
+			inputText.Focus();
+		}
 
-        private void scrambleBtn_Click(object sender, EventArgs e)
-        {
-            scrmbledText.Text =
-                ns7.sadhontoon.Scrambler.scrableTheText(
-                inputText.Text,
-                AllUpperCase.Checked,
-                AllLowerCase.Checked,
-                TrimSpace.Checked,
-                insertSpace.Checked);
-        }
+		private void scrambleBtn_Click(object sender, EventArgs e) {
+			scrmbledText.Text = ns7.sadhontoon.Scrambler.scrableTheText(inputText.Text, AllUpperCase.Checked, AllLowerCase.Checked, TrimSpace.Checked, insertSpace.Checked);
+		}
 
-        private void AllUpperCase_CheckedChanged(object sender, EventArgs e)
-        {
-            AllLowerCase.Enabled = !AllUpperCase.Checked;
-        }
+		private void AllUpperCase_CheckedChanged(object sender, EventArgs e) {
+			AllLowerCase.Enabled = !(AllUpperCase.Checked);
+		}
 
-        private void AllLowerCase_CheckedChanged(object sender, EventArgs e)
-        {
-            AllUpperCase.Enabled = !AllLowerCase.Checked;
-        }
+		private void AllLowerCase_CheckedChanged(object sender, EventArgs e) {
+			AllUpperCase.Enabled = !(AllLowerCase.Checked);
+		}
 
-        private void inputText_Click(object sender, EventArgs e)
-        {
-            if (!inputAnyText)
-            {
-                inputText.Text = "";
-                inputAnyText = true;
-            }
-        }
-    }
+		private void inputText_Click(object sender, EventArgs e) {
+			if (inputAnyText == false) {
+				inputText.Text = "";
+				inputAnyText = true;
+			}
+		}
+	}
 }
--- Scrambler/Scrambler.cs
+++ Scrambler/Scrambler.cs
@@ -1,60 +1,46 @@
 ﻿using System;
-namespace ns7.sadhontoon
-{
-    class Scrambler
-    {
-        public static string scrableTheText(string text, bool allUp, bool allLower, bool trimSpace, bool insertSpace)
-        {
-            System.Random random = new System.Random();
-            char[] set = text.ToCharArray();
-            int count = set.Length;
-            if (allUp)
-            {
-                for (int i = 0; i < set.Length; i++)
-                {
-                    if (Char.IsLower(set[i])) set[i] = Char.ToUpper(set[i]);
-                }
-            } 
-            if (allLower)
-            {
-                for (int i = 0; i < set.Length; i++)
-                {
-                    if (Char.IsUpper(set[i])) set[i] = Char.ToLower(set[i]);
-                }
-            }
-            if (trimSpace)
-            {
-                for (int i = 0; i < set.Length; i++)
-                {
-                    if (Char.IsWhiteSpace(set[i]))
-                    {
-                        while (Char.IsWhiteSpace(set[--count])) ;
-                        set[i] = set[count];
-                    }
-                }
-            }
-            System.Text.StringBuilder sbt = new System.Text.StringBuilder();
-            int a;
-            if (insertSpace)
-            {
-                while (count > 0)
-                {
-                    a = random.Next(count);
-                    sbt.Append(set[a]);
-                    sbt.Append(' ');
-                    set[a] = set[--count];
-                }
-            }
-            else
-            {
-                while (count > 0)
-                {
-                    a = random.Next(count);
-                    sbt.Append(set[a]);
-                    set[a] = set[--count];
-                }
-            }
-            return sbt.ToString();
-        }
-    }
+
+namespace ns7.sadhontoon {
+	class Scrambler {
+		public static string scrableTheText(string text, bool allUp, bool allLower, bool trimSpace, bool insertSpace) {
+			System.Random random = new System.Random();
+			char[] set = text.ToCharArray();
+			int count = set.Length;
+			if (allUp) {
+				for (int i = 0; i < set.Length; i++) {
+					if (Char.IsLower(set[i])) set[i] = Char.ToUpper(set[i]);
+				}
+			}
+			if (allLower) {
+				for (int i = 0; i < set.Length; i++) {
+					if (Char.IsUpper(set[i])) set[i] = Char.ToLower(set[i]);
+				}
+			}
+			if (trimSpace) {
+				for (int i = 0; i < set.Length; i++) {
+					if (Char.IsWhiteSpace(set[i])) {
+						while (Char.IsWhiteSpace(set[--count])) ;
+						set[i] = set[count];
+					}
+				}
+			}
+			System.Text.StringBuilder sbt = new System.Text.StringBuilder();
+			int a;
+			if (insertSpace) {
+				while (count > 0) {
+					a = random.Next(count);
+					sbt.Append(set[a]);
+					sbt.Append(' ');
+					set[a] = set[--count];
+				}
+			} else {
+				while (count > 0) {
+					a = random.Next(count);
+					sbt.Append(set[a]);
+					set[a] = set[--count];
+				}
+			}
+			return sbt.ToString();
+		}
+	}
 }
